7084b6cdcc891a968844723765c0d429c9cad4df
dan523 <danwright523@gmail.com>
2016-07-04 21:38:12 -0400

    Adding SymbolLookup component stub and pulling ngMaterial into the project.

---
+++ app/components/symbol-lookup/symbol-lookup.component.js
@@ -0,0 +1,11 @@
+/// <reference path="../../../typings/index.d.ts" />
+'use strict';
+
+angular
+    .module('portfolio')
+    .component('symbolLookup', {
+        templateUrl: 'components/symbol-lookup/symbol-lookup.template.html',
+        controller: function SymbolLookup () {
+
+        }
+    });
