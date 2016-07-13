7df539a61268de09b90c37fa280d5c99cf10b1a0
Wright <dwright@dayforce.com>
2016-07-13 10:35:51 -0400

    Basic auto-complete usage with test data

--- app/components/symbol-lookup/symbol-lookup.component.js
+++ app/components/symbol-lookup/symbol-lookup.component.js
@@ -6,6 +6,22 @@ angular
     .component('symbolLookup', {
         templateUrl: 'components/symbol-lookup/symbol-lookup.template.html',
         controller: function SymbolLookup () {
+            var self = this;
+            self.test = [
+                {display: 'AA'},
+                {display: 'BB'},
+                {display: 'CC'},
+                {display: 'DD'},
+                {display: 'AB'},
+                {display: 'AC'},
+                {display: 'AD'}                
+            ]
 
+            self.getMatches = function getMatches(searchString) {
+                var upperCaseSearchStr = searchString.toUpperCase();
+                return self.test.filter(function(item) {
+                    return item.display.toUpperCase().indexOf(upperCaseSearchStr) >= 0;
+                });
+            };
         }
     });
