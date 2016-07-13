64bac8ab292278935b7d60c9686d441ca3771ac4
Wright <dwright@dayforce.com>
2016-07-13 15:55:37 -0400

    Make item update async using promises. Fixed CSS.

--- app/components/symbol-lookup/symbol-lookup.component.js
+++ app/components/symbol-lookup/symbol-lookup.component.js
@@ -8,17 +8,18 @@ angular
         controller: ['Symbol',
             function SymbolLookup (Symbol) {                
                 var self = this;
-                self.matches = [];
-                self.updateMatches = function updateMatches(searchString) {
-                    Symbol.get({
-                            query: searchString,
-                            region: 'CA',   // TODO: Don't hardcode
-                            lang: 'en-CA'   // TODO: Don't hardcode
-                        }, 
-                        function (results) {
-                            self.matches = results.ResultSet.Result;
-                        }
-                    );                    
+
+                self.getMatches = function updateMatches(searchString) {                    
+                    return Symbol.get({
+                                query: searchString,
+                                region: 'CA',   // TODO: Don't hardcode
+                                lang: 'en-CA'   // TODO: Don't hardcode
+                            }
+                        )
+                        .$promise
+                        .then(function (results) {
+                            return results.ResultSet.Result;
+                        });
                 };
             }
         ]
